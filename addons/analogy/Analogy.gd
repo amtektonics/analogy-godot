5eaa207f7dff203a0c577bbc7aa7c94acf0aa28b
Andrew McGuire <amtektonics@gmail.com>
2026-02-04 23:17:49 -0500

    added hardware logging for basic godot stuff

--- addons/analogy/Analogy.gd
+++ addons/analogy/Analogy.gd
@@ -8,23 +8,30 @@ var _message_stack = []
 
 var _requesting = false
 
+#This can be replaced when new client data types are added on the server side
+enum client_data_types{
+	Unkown,
+	Processor,
+	GraphicsCard,
+	Motherboard,
+	RAM,
+	OperatingSystem,
+	ModelName
+}
+
+
+
 func _ready():
 	_http_request = HTTPRequest.new()
 	add_child(_http_request)
 	_http_request.request_completed.connect(_on_request_completed)
-	
-	
-	log_information("App Started")
-	log_warnining("Test warninig")
-	log_error("Test error")
-	log_client_data("Test data")
 
 
 func _physics_process(delta: float) -> void:
 	if(!_requesting):
 		if(_message_stack.size() > 0):
 			var dat = _message_stack.pop_front()
-			if(dat.has("path")  && dat.has("client_id") && dat.has("message")):
+			if(dat.has("path") && dat.has("client_id") && dat.has("message")):
 				var status = _http_request.request(_api_url+"/" + dat["path"], ["Content-Type: application/json"], HTTPClient.METHOD_POST, JSON.stringify(dat))
 				_requesting = true
 
@@ -52,15 +59,27 @@ func log_error(message:String):
 		}
 	_message_stack.append(json)
 
-func log_client_data(message:String):
+func log_client_data(type:client_data_types, message:String):
 	var json = {
 			"client_id": get_unique_machine_id(),
 			"message": message,
+			"type":type,
 			"path": "logClientData"
 		}
 	_message_stack.append(json)
 
 
+func log_hardware():
+	log_client_data(client_data_types.Processor, OS.get_processor_name())
+	
+	log_client_data(client_data_types.GraphicsCard, str(OS.get_video_adapter_driver_info()))
+
+	var ram = OS.get_memory_info()["physical"]
+	log_client_data(client_data_types.RAM, str(ram))
+	
+	log_client_data(client_data_types.OperatingSystem, str(OS.get_name()))
+	
+	log_client_data(client_data_types.ModelName, str(OS.get_model_name()))	
 
 func get_unique_machine_id():
 	return OS.get_unique_id().replace("{", "").replace("}", "")
